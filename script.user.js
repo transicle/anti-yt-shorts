393a32c207183ee59fbaeb504e6954a934380a24
lily.transgirls.win <ilovesuno@proton.me>
2026-01-31 01:49:49 -0700

    chore: upload just a simple test rn

---
+++ script.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        anti-yt-shorts
+// @namespace   https://github.com/transicle/anti-yt-shorts
+// @match       https://youtube.com/
+// @grant       none
+// @version     1.0
+// @author      transicle
+// @description Avoid distractions by completely removing the YouTube Shorts feature from YouTube.
+// ==/UserScript==
+
+(async () => {
+	alert("hello youtube");
+})();
